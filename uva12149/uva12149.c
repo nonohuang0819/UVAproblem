e45d72e6db92d9322d25536688adeb084dd70c7d
nonohuang <yh7624908@gmail.com>
2023-07-05 12:54:28 +0800

    update

--- uva12149/uva12149.c
+++ uva12149/uva12149.c
@@ -4,13 +4,12 @@
 int main(){
     int which;
     while(scanf("%d", &which) != EOF){
+        if(which == 0) continue;
         int n = 0;
-        
         for(int i = 1; i <= which; i++){
             n += i * i;
         }
         printf("%d\n", n);
-        
     }
     
     return 0;
